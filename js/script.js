54ddd26538ba4b582d368cb5168c347aa643f12f
Olúwatìmìlé̩hìn Ako̩gun <hello@timiakogun.com>
2023-01-24 10:25:11 +0100

    feat: Add charcoal payment as accordion title

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
+
 function isExpandedCC() {
-    return document.getElementById('cc').getAttribute('aria-expanded')
+    return getAriaExpanded("cc") == "true"
+}
+
+function getAriaExpanded(id) {
+    return document.getElementById(id).getAttribute('aria-expanded');
 }
 
 function charcoalChecked() {
-    return document.getElementById('charcoal').checked
+    return getAriaExpanded("charcoal") == "true"
 }
 
 function handleSubmit(e) {
     document.getElementById('card-alert').classList.remove("show")
-    if (isExpandedCC() == "true"){
+    if (isExpandedCC()){
         document.getElementById('card-alert').classList.add("show")
     }
-
+    
     if (charcoalChecked()) {
         window.location.replace("./charcoal.html");
     }
 }
+
+
+setTimeout(function () {
+    // "Pay &#8358;7,500 with Charcoal <img class="icon" src="images/charcoal.png" alt="">"
+    let charcoalPricingContent= document.createElement("span")
+    let charcoalLogo = document.createElement("img")
+    charcoalLogo.classList = "icon"
+    charcoalLogo.src = "images/charcoal.png"
+    const charcoalText = "Pay &#8358;7,500 with Charcoal"
+    charcoalPricingContent.innerHTML = charcoalText
+    charcoalPricingContent.appendChild(charcoalLogo)
+    document.getElementById("charcoal-text").appendChild(charcoalPricingContent)
+}, 1000);
